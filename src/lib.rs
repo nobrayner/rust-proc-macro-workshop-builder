697483f5d20c72c4f5d3ae5e62236fc21b3ab784
Braydon Hall <40751395+nobrayner@users.noreply.github.com>
2022-06-04 17:58:22 +1000

    feat: 04-call-build passing

--- src/lib.rs
+++ src/lib.rs
@@ -8,17 +8,20 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let ast: syn::DeriveInput = syn::parse(input).unwrap();
 
     let syn::DeriveInput { ident, data, .. } = ast;
+    let struct_ident = ident;
 
     if let syn::Data::Struct(data_struct) = data {
-        let builder_ident = format_ident!("{}Builder", ident);
+        let builder_ident = format_ident!("{}Builder", struct_ident);
         let syn::DataStruct { fields, .. } = data_struct;
 
         if let syn::Fields::Named(fields) = fields {
             let named_fields = fields.named;
 
             let mut initial_values: Vec<TokenStream2> = vec![];
-            let mut field_declarations: Vec<TokenStream2> = vec![];
-            let mut builder_functions: Vec<TokenStream2> = vec![];
+            let mut declarations: Vec<TokenStream2> = vec![];
+            let mut functions: Vec<TokenStream2> = vec![];
+            let mut checks: Vec<TokenStream2> = vec![];
+            let mut struct_fields: Vec<TokenStream2> = vec![];
 
             for field in named_fields.iter() {
                 let syn::Field { vis, ident, ty, colon_token, .. } = field;
@@ -26,19 +29,33 @@ pub fn derive(input: TokenStream) -> TokenStream {
                 initial_values.push(quote! {
                     #ident: None
                 });
-                field_declarations.push(quote! {
+
+                declarations.push(quote! {
                     #vis #ident #colon_token Option<#ty>
                 });
-                builder_functions.push(quote! {
+
+                functions.push(quote! {
                     fn #ident(&mut self, #ident: #ty) -> &mut Self {
                         self.#ident = Some(#ident);
                         self
                     }
                 });
+
+                // FIXME: Actually add ident as a string, not as the literal "#ident"
+                let error_message = format!("Must provide {}", ident.as_ref().unwrap());
+                checks.push(quote! {
+                    if let None = self.#ident {
+                        return Err(Box::from(#error_message));
+                    }
+                });
+
+                struct_fields.push(quote! {
+                    #ident: self.#ident.unwrap()
+                });
             }
 
             let expanded = quote! {
-                impl #ident {
+                impl #struct_ident {
                     pub fn builder() -> #builder_ident {
                         #builder_ident {
                             #(#initial_values),*
@@ -46,10 +63,17 @@ pub fn derive(input: TokenStream) -> TokenStream {
                     }
                 }
                 pub struct #builder_ident {
-                    #(#field_declarations),*
+                    #(#declarations),*
                 }
                 impl #builder_ident {
-                    #(#builder_functions)*
+                    #(#functions)*
+                    pub fn build(mut self) -> Result<#struct_ident, Box<dyn std::error::Error>> {
+                        #(#checks)*
+
+                        Ok(#struct_ident {
+                            #(#struct_fields),*
+                        })
+                    }
                 }
             };
 
